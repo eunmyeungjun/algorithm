46a33af466b5e4e361e44e198ea3c28d2f13d873
eunmyeungjun <audwns419@naver.com>
2023-10-15 20:32:20 +0900

    톱니바퀴

--- src/main/kotlin/implementation/14891 톱니바퀴.kt
+++ src/main/kotlin/implementation/14891 톱니바퀴.kt
@@ -1,28 +1,149 @@
 package implementation
 
-import java.util.LinkedList
+import java.util.*
 
-fun main()=with(System.`in`.bufferedReader()) {
+fun main()= with(System.`in`.bufferedReader()){
+    val gears = Array<LinkedList<Char>>(4) { LinkedList() }
+    for (i in 0 until 4) {
+        val gearNum = readln()
+        for (char in gearNum) {
+            gears[i].add(char)
+        }
+    }
+    val k = readln().toInt()
 
-    //val gearfirst = LinkedList<Int>().add(readln().toInt())
-    //val gearsecond = LinkedList<Int>().add(readln().toInt())
-    //val gearthird = LinkedList<Int>().add(readln().toInt())
-    //val gearforth = LinkedList<Int>().add(readln().toInt())
-    val gearfirst =LinkedList<Int>(listOf<Int>(readln().toInt()))
-    val gearsecond =LinkedList<Int>()
-    val gearthird =LinkedList<Int>()
-    val gearforth =LinkedList<Int>()
+    for(i in 0 until k){
+        val plan = readln().split(" ")
+        val whatGear=plan[0].toInt() - 1
+        val direction = plan[1].toInt()
+        if(direction==1){
+            when(whatGear){
+                0->if (gears[0][2]==gears[1][6]){
+                    rotate(gears,0)
+                    break
+                }
+                else{
+                    if (gears[1][2]==gears[2][6]){
+                        rotate(gears,0)
+                        deRotate(gears,1)
+                        break
+                    }
+                    else {
+                        if (gears[2][2] == gears[3][6]) {
+                            rotate(gears, 0)
+                            deRotate(gears, 1)
+                            rotate(gears, 2)
+                            break
+                        } else {
+                            rotate(gears, 0)
+                            deRotate(gears, 1)
+                            rotate(gears, 2)
+                            deRotate(gears, 3)
+                            break
+                        }
+                    }
 
-    for(i in 0..4){
+                }
+                1->if(gears[0][2]==gears[1][6]){
+                    rotate(gears,1)
+                    continue
+                }
+                    else {rotate(gears,1)
+                    deRotate(gears,0)
+                    continue
+
+
+                }
+
+            }
+
+
+
+
+            }
+        }
 
     }
 
-
-    println(gearfirst)
-    println(gearsecond)
-    println(gearthird)
-    println(gearforth)
-
-
+fun rotate(gears : Array<LinkedList<Char>>,i :Int,) {
+    gears[i].addFirst(gears[i].removeLast())
 }
+fun deRotate(gears: Array<LinkedList<Char>>, i: Int){
+    gears[i].addLast(gears[i].removeLast())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 이거 코드가 너무너무너무 길어지는데?
+fun main()=with(System.`in`.bufferedReader()) {
+    val gearFirst = readln().toMutableList().toCharArray()
+    val gearSecond = readln().toMutableList().toCharArray()
+    val gearThird = readln().toMutableList().toCharArray()
+    val gearFourth = readln().toMutableList().toCharArray()
+    val k = readln().toInt()
+
+
+    for (i in 0 until k) {
+        val (x, y) = readln().split(" ")
+        val whatGear = x.toInt()
+        val dircetion = y.toInt()
+        when (whatGear) {
+            1 -> if (dircetion == 1) {
+                if (gearFirst[2] == gearSecond[6]) {
+                    gearFirst.add(gearFirst
+                } else {
+                    if (gearSecond[2] == gearThird[6]) {
+                        gearSecond.plus(gearSecond.drop(0))
+
+                    } else {
+
+                    }
+                }
+            }
+            2->{}
+            3->{}
+            4-> {}
+
+        }
+    }
+}
+*/
+
+
+
+
 
--- src/main/kotlin/implementation/main.kt
+++ src/main/kotlin/implementation/main.kt
@@ -1,18 +1,9 @@
 package implementation
 
-fun main()=with(System.`in`.bufferedReader()) {
-    val paperCount = readln().toInt()
-    val whitePaper = Array(100) { BooleanArray(100) }
-    var answer = 0
-repeat(paperCount){
-    val(x,y)= readln().split(" ").map { it.toInt() }
-    for (i in x until x + 10) {
-        for (j in y until y + 10) {
-            if (whitePaper[i][j]) continue
-            whitePaper[i][j] = true
-            answer++
-        }
-    }
-    }
-    println(answer)
+fun main()=with(System.`in`.bufferedReader()){
+    val(x,y)= readln().split(" ")
+
+    println(x)
+    println(y)
+
 }
