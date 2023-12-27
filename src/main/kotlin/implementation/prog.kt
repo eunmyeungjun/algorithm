4a0176b3798e02f1544dff1020d393bcd479419e
eunmyeungjun <audwns419@gmail.com>
2023-12-27 21:15:46 +0900

    prog

---
+++ src/main/kotlin/implementation/prog.kt
@@ -0,0 +1,20 @@
+package implementation
+
+import java.util.*
+
+
+fun main()=with(System.`in`.bufferedReader()) {
+    val n = readln().toInt()
+    val que: Queue<Int> = LinkedList<Int>()
+
+    for (i in 1..n) {
+        que.add(i)
+    }
+    while(que.size !=1){
+        que.poll()
+        que.add(que.poll())
+    }
+    println(que.poll())
+}
+
+
